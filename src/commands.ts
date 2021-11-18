423abba54b566d763bea1353fdd75826ce4e7929
MasonBone <mason.bone1@gmail.com>
2021-11-18 11:38:17 -0600

    Added webpage

--- src/commands.ts
+++ src/commands.ts
@@ -83,12 +83,43 @@ function resetEditorScale(): void {
 
 function openWebview(): void {
   //vscode.commands.executeCommand('workbench.action.zoomOut');
-  vscode.window.createWebviewPanel(
-    'catCoding', // Identifies the type of the webview. Used internally
-    'Cat Coding', // Title of the panel displayed to the user
+  const panel = vscode.window.createWebviewPanel(
+    'mindReader', // Identifies the type of the webview. Used internally
+    'Mind Reader', // Title of the panel displayed to the user
     vscode.ViewColumn.One, // Editor column to show the new webview panel in.
     {}
   ); // Webview options. More on these later.
+
+  panel.webview.html = getWebviewContent();
+}
+
+function getWebviewContent() {
+  return `<!DOCTYPE html>
+  <html lang="en">
+  <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Mind Reader</title>
+  </head>
+  <body>
+      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT6a4XaqHkKcxJ6ZFms1RNrRurcOfl-diW90DAdpAx0Kv-rtrLJXovIhcUpayqFHATkrQ&usqp=CAU" width="600" />
+      <p></p>
+      <h1>Welcome to Mind_Reader!</h1>
+      <p></p>
+      <p>We are the Single Semester Snobs and this is our Tool to Help Blind Students Program Lego Mindstorms Robots in Python.</p>
+      <p>This tool includes features such as a hotkey that says how many spaces in the text starts, an Accessibility Pane, </p>
+      <p>Audio Alerts, and an advanced settings window. The tool has hotkeys for both PC and Mac commands. </p>
+      <p>This system is intended for everyone, but primarily for students K-12 who are visually impaired. </p>
+      <p>Our goal is to provide an enhanced experience for students who are visually impaired that is transparent to sighted students. </p>
+      <p>This allows for everyone to use the same software solution, whether or not they are vision impaired. </p>
+      <p></p>
+      <h2>This is the Lego Spike Prime!</h2z>
+      <p></p>
+      <img src="https://cdn.vox-cdn.com/thumbor/qoaa6N2ppl7oj97MR-aj43qPy0w=/0x0:1024x576/920x613/filters:focal(431x207:593x369):format(webp)/cdn.vox-cdn.com/uploads/chorus_image/image/63339099/lego_spike.0.png" width="300" />
+      <p></p>
+      <a href="https://www.lego.com/en-us/product/lego-education-spike-prime-set-45678">Get the robot!</a>
+  </body>
+  </html>`;
 }
 
 export default commands;
