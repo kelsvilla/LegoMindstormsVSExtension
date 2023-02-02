c3b683e702d08f25c7c35beac4354fd7ce3359e6
Jigme Sherpa <jigmesherpa@my.unt.edu>
2023-02-01 22:49:12 -0600

    added comments to help other developers in the team

--- src/commands/suggestionFilter.ts
+++ src/commands/suggestionFilter.ts
@@ -1,12 +1,21 @@
 import * as vscode from 'vscode';
 
+
 function getReplaceIndex(line:string){
-        const position = line.search(/\./);
+  /**
+   * utility function to retrive the index of '.' in a given line
+   * @param line the string in current line where the completion was requested
+   */
+        var position = line.search(/\./);
         if(position!==0){
           return position +1;
         }
+        if(line.search('=')!==0){ return line.search('=') + 1;}
+
+        //if '.' not found then return zero
         return 0;
 }
+//function to auto complete requested text
 export async function suggestionFilter() {
 	const activeEditor = vscode.window.activeTextEditor;
 	if (!activeEditor) {
@@ -18,9 +27,9 @@ export async function suggestionFilter() {
 		activeEditor.document.uri,
 		activeEditor.selection.active,
 	);
+  //get the current line where cursor is pointed
   var lineAt = activeEditor.selection.active;
-  console.log("sussgestion at line:",lineAt);
-  console.log(activeEditor.document.lineAt(lineAt).text);
+
 	//categorize suggestions
 	var functions:vscode.QuickPickItem[] = [];
 	var methods:vscode.QuickPickItem[] = [];
@@ -32,7 +41,10 @@ export async function suggestionFilter() {
   var modules:vscode.QuickPickItem[] = [];
   var property:vscode.QuickPickItem[] = [];
 	/* although the text editor marks the following code as error,
-	no issues are detected while running the program and generates expected items */
+	no issues are detected while running the program and generates expected results */
+
+  //item.kind is a inbuilt property defined by the langugage server
+  //fill the categories based on its kind
 	for(var item of completion.items){
      if(item.kind===1){
       methods.push(item);
@@ -101,7 +113,9 @@ export async function suggestionFilter() {
       selectQp.items = variables;
     }
     //todo -- harish
+
     //follow the above if else pattern and complete the rest
+
 		selectQp.show();
 		selectQp.onDidAccept(()=>{
 		activeEditor.edit(editBuilder =>{
@@ -109,11 +123,16 @@ export async function suggestionFilter() {
     var startPosition = new vscode.Position(lineAt.line,replacePosition);
     var endPosition = new vscode.Position(lineAt.line,activeEditor.document.lineAt(lineAt).text.length);
     var range = new vscode.Range(startPosition,endPosition);
+    //delete first
     editBuilder.delete(range);
+    //then replace
 		editBuilder.replace(startPosition,selectQp.selectedItems[0].label);
 		selectQp.dispose();
 		}).then(success=>{
       if(success){
+        /*the editor selects the entire texts that has been completed which causes the complete text to be replaced
+        if we immediatedly start typing after completion. To solve this create a new selection with position marking
+        the end of the completed text*/
         var postion = activeEditor.selection.end;
         activeEditor.selection = new vscode.Selection(postion, postion);
       }
