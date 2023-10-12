6f1f9b2fd91013a609486cab4ea01db81471b030
chauler <amt1309@gmail.com>
2023-10-12 11:47:25 -0500

    Removed harcoded backslash

--- src/client01.ts
+++ src/client01.ts
@@ -7,8 +7,7 @@ import * as path from 'path';
 
  function activateVoiceServer(){
     //activate server
-    
-    const interpretor = path.join(path.normalize(rootDir).replace(`${path.sep}out`, ''), 'voice-server-setup', 'venv', 'bin', 'python');
+    const interpretor = path.normalize(rootDir).replace(`${path.sep}out`, path.join(`${path.sep}voice-server-setup`,'venv','Scripts','python'))
     const defaults = {
       cwd: rootDir.replace(`${path.sep}out`,''),
       //env: process.env,
