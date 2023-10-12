49bec1edd506473d0268e5fdce26ea7dc0bfaf33
Bryan Tang <bryanktang@gmail.com>
2023-10-12 16:53:45 -0500

    Add portaudio dependency and edit macos set up

--- voice-server-setup/initial_set_up_macos.sh
+++ voice-server-setup/initial_set_up_macos.sh
@@ -8,12 +8,7 @@ then
     ELEVATE='sudo';
 fi
 cd $ABSPATH
-if [ -d "venv" ]
-then 
-    echo "Directory venv already exists."
-else
-    python3 -m venv venv
-    source venv/bin/activate
-    which python
-    pip install -r requirements.txt
-fi
+python3 -m venv venv
+source venv/bin/activate
+which python
+pip install -r requirements.txt
