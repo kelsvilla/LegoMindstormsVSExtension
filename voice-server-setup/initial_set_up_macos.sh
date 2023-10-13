27116314dc4f950db72080e50c4fadfbe03344a8
Alex Tomjack <amt1309@gmail.com>
2023-10-12 20:19:31 -0500

    Added include libraries to pip install

--- voice-server-setup/initial_set_up_macos.sh
+++ voice-server-setup/initial_set_up_macos.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 ABSPATH=$(cd "$(dirname "$0")"; pwd -P)
-ROOTDIR=$(cd "$(dirname "$0")/.."; pwd -P)
+ROOTDIR=(cd "$(dirname "$0")/.."; pwd -P)
 ELEVATE='';
 if (( $UID !=0 )); 
 then 
@@ -11,4 +11,5 @@ cd $ABSPATH
 python3 -m venv venv
 source venv/bin/activate
 which python
-LDFLAGS="-L/opt/homebrew/Cellar/portaudio/19.7.0/lib" CFLAGS="-I/opt/homebrew/Cellar/portaudio/19.7.0/include" pip install -r requirements.txt
+pip install --global-option='build_ext' --global-option="-I$ROOTDIR/dependencies/portaudio/19.7.0/include" --global-option="-L$ROOTDIR/dependencies/portaudio/19.7.0/lib" requirements.txt
+#pip install -r requirements.txt
