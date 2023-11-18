1825210fa2d0c53a0851e8908b8b056ec80072ba
K3V1N-G <123893629+K3V1N-G@users.noreply.github.com>
2023-11-17 22:59:41 -0600

    Added parser to midi functions
    Used parser code from the line scope functionality to get context string for sound cues

--- src/commands/midi.ts
+++ src/commands/midi.ts
@@ -1,79 +1,128 @@
-import * as vscode from "vscode";
-
+import { TextEditorSelectionChangeEvent, window, workspace } from "vscode";
+import pl     = require("../pylex");
 import { CommandEntry } from './commandEntry';
 import * as jzz from 'jzz';
 
 export const midicommands: CommandEntry [] = [
 {
-    name: 'mind-reader.soundCuesSwitch',
-    callback: soundCuesSwitch,    },
+    name: 'mind-reader.toggleSoundCues',
+    callback: toggleSoundCues,    },
 ];
 
-
+//TODO: Create hotkey for activating audio cues
 
 let shouldPlayMIDINote = false;
 
-function soundCuesSwitch(){
-
+function toggleSoundCues(){
     shouldPlayMIDINote = !shouldPlayMIDINote;
 }
 
-vscode.window.onDidChangeTextEditorSelection(playMidi);
+window.onDidChangeTextEditorSelection(playerContext);
 
-function playMidi(_event: vscode.TextEditorSelectionChangeEvent){
+function playMidi(contextString: string){
     var output = jzz().openMidiOut();
-    //output.send(0x90, 'C#5', 127).wait(500).send(0x90, 'f#5', 127);
-
-    const editor = vscode.window.activeTextEditor;
-    if(editor && shouldPlayMIDINote){
-        const currentPosition = editor.selection.active;
-        const currentLine = editor.document.lineAt(currentPosition.line).text;
-
-        var chordType : string = lineContext(currentLine, currentPosition.line);
-        output.note(0, chordType, 127, 550);
-    }
-
+    var chordType : string = lineContext(contextString);
+    //window.showInformationMessage(contextString);
+    output.note(0, chordType, 127, 550);
 }
 
+function playerContext(_event: TextEditorSelectionChangeEvent){
+    const editor = window.activeTextEditor;
+
+    if (editor && shouldPlayMIDINote) {
+        // current text and line number
+        const editorText: string    = editor.document.getText();
+        const line      : number    = editor.selection.active.line;
+        // get tab info settings
+        const size      : number    = typeof editor.options.tabSize === 'number'? editor.options.tabSize: 4;
+        const hard      : boolean   = !editor.options.insertSpaces;
+        // initialize parser
+        const parser    : pl.Parser = new pl.Parser(editorText, {
+            size,
+            hard
+        });
+
+        parser.parse();
+        const context: pl.LexNode[] = parser.context(line);
+        // build text
+        const contextString: string = createContextString(context);
+        playMidi(contextString);
+    }
+}
+
+function createContextString(context: pl.LexNode[]): string {
+    if (context.length < 1) {
+        throw new Error('Cannot create context string for empty context');
+    }
+
+    let contextString: string = ``; // 1 based
+    // Print the current line
+    if (context[0].token && context[0].token.attr) {
+        let tokenTypeString: string = `${context[0].token.type.toString()}`;
+        contextString += `${tokenTypeString !== pl.PylexSymbol.STATEMENT?tokenTypeString:""
+        } ${context[0].token.attr.toString()}`;
+    }
+
+    for (let i: number = 1; i < context.length; i++) {
+        const node: pl.LexNode = context[i];
+        const inside: string = "inside";
+        // Node contains information relevant to the current line
+        if (node.token && node.token.type !== pl.PylexSymbol.EMPTY &&
+            node.token.type !== pl.PylexSymbol.STATEMENT) {
+            contextString += ` ${inside} ${node.token.type.toString()}`;
+            if (node.token.attr) {
+                contextString += ` ${node.token.attr.toString()}`;
+            }
+        }
+        // Node is the document root
+        if (node.label === 'root') {
+            // Append the name (relative path) of the document in the workspace
+            if (window.activeTextEditor?.document.uri) {
+                contextString += ` ${inside} ${workspace.asRelativePath(window.activeTextEditor?.document.uri)}`;
+            } else {
+                contextString += ` ${inside} the Document`;
+            }
+            continue;
+        }
+    }
+
+    return contextString;
+}
+
+
+
 // Function checking for nested for loop
-function lineContext(currentLine: string, currentLineNumber: number): string {
-    const editor = vscode.window.activeTextEditor;
-    const indentation = currentLine.match(/^\s*/)?.[0] || '';
+function lineContext(contextString: string): string {
 // Checking for case
-    const forIndex = currentLine.indexOf('for');
-    if (forIndex !== -1) {
-      // Check if the 'for' keyword is indented more than the previous line
-        const lines = editor?.document.getText().split('\n') || [];;
+    const forIndex = contextString.indexOf('for');
+    var forCount = countOccurrences(contextString, 'for');
 
-        if (currentLineNumber > 0) {
-            const previousLine = lines[currentLineNumber - 1];
-            const prevForIndex = previousLine.indexOf('for');
-            if(prevForIndex !== -1){
-                const previousIndentation = previousLine.match(/^\s*/)?.[0] || '';
-                if (indentation.length > previousIndentation.length) {
-                    return 'd4'; //Returning nested for loop note
-                }
-            }
+    const ifIndex = contextString.indexOf('if');
+
+    if(forIndex !== -1){
+        if(forIndex === 0){
+            let noteNum = forCount + 2;
+            return 'a' + noteNum;
         }
-        return 'c4'; // Returning regular for loop note
     }
-    const ifIndex = currentLine.indexOf('if');
     if(ifIndex !== -1){
-        // Check if the 'for' keyword is indented more than the previous line
-        const lines = editor?.document.getText().split('\n') || [];;
-
-        if (currentLineNumber > 0) {
-            const previousLine = lines[currentLineNumber - 1];
-            const prevForIndex = previousLine.indexOf('if');
-            if(prevForIndex !== -1){
-                const previousIndentation = previousLine.match(/^\s*/)?.[0] || '';
-                if (indentation.length > previousIndentation.length) {
-                    return 'f4'; //Returning nested for loop note
-                }
-            }
+        if(ifIndex === 1){
+            let noteNum = ifIndex + 2;
+            return 'b' + noteNum;
         }
-        return 'e4'; // Returning regular for loop note
+    }
+    return 'g4';
+}
+
+  function countOccurrences(inputString: string, substring: string): number {
+    let count = 0;
+    let index = inputString.indexOf(substring);
+
+    while (index !== -1) {
+      count++;
+      index = inputString.indexOf(substring, index + 1);
     }
 
-    return 'g4';
+    return count;
   }
+
