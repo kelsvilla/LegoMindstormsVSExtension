cf022e27ea13b43ede2a05a38986110de70ee252
Alex Tomjack <amt1309@gmail.com>
2023-12-03 21:59:23 -0600

    Fix formatting issues after merging with main.

--- src/client01.ts
+++ src/client01.ts
@@ -34,51 +34,52 @@ function activateVoiceServer() {
 			"pipe", // pipe child's stderr to a parent.
 		],
 	};
-	//console.log('test: ${workspaceFolder}');
 	//start voice server
 	const server = spawn(interpretor, ["server.py"], defaults);
 	vscode.window.showInformationMessage("Server Started");
 	return server;
 }
 
-function runClient()
-{
-    let socket: WebSocket | null = null;
-    const socketPort = 12152;
-    socket = new WebSocket(`ws://localhost:${socketPort}`);
-    console.log('[client] socket created .. ');
+function runClient() {
+	let socket: WebSocket | null = null;
+	const socketPort = 12152;
+	socket = new WebSocket(`ws://localhost:${socketPort}`);
+	console.log("[client] socket created .. ");
 
-    socket.addEventListener('open', (event) => {
-        console.log('[client] is ready to communicate..', event.target.OPEN);
-    });
+	socket.addEventListener("open", (event) => {
+		console.log("[client] is ready to communicate..", event.target.OPEN);
+	});
 
-    socket.on('message', (message) => {
-        console.log('message recived', message);
-        if (message.toString()!=='Shutting down voice commands.') {
-          if(message.toString().split(',').length !== 2) return; //Ensures both vars below will be defined with some value
-          const [cmdName, logMsg] = message.toString().split(',');
-          vscode.commands.executeCommand(cmdName).then(
-            () => {
-              console.log('command to run success:',cmdName);
-              setTimeout(() => {
-                vscode.window.showInformationMessage(logMsg).then(
-                  (success) => {
-                    console.log(success);
-                  }, 
-                  (reject) => {
-                    console.log(reject);
-                  });
-              }, 5);
-            }, 
-            (reject) => {
-              vscode.window.showInformationMessage("Action Cannot be Completed!");
-              console.log(reject);
-            });
-        }
-        else{
-          vscode.window.showInformationMessage("Exiting Voice Commands.");
-        }
-    });
+	socket.on("message", (message) => {
+		console.log("message recived", message);
+		if (message.toString() !== "Shutting down voice commands.") {
+			if (message.toString().split(",").length !== 2) return; //Ensures both vars below will be defined with some value
+			const [cmdName, logMsg] = message.toString().split(",");
+			vscode.commands.executeCommand(cmdName).then(
+				() => {
+					console.log("command to run success:", cmdName);
+					setTimeout(() => {
+						vscode.window.showInformationMessage(logMsg).then(
+							(success) => {
+								console.log(success);
+							},
+							(reject) => {
+								console.log(reject);
+							},
+						);
+					}, 5);
+				},
+				(reject) => {
+					vscode.window.showInformationMessage(
+						"Action Cannot be Completed!",
+					);
+					console.log(reject);
+				},
+			);
+		} else {
+			vscode.window.showInformationMessage("Exiting Voice Commands.");
+		}
+	});
 }
 
 export function startStreaming() {
@@ -96,7 +97,7 @@ export function startStreaming() {
 		//server ready for connection
 		var serverMsg = data.toString();
 		if (data.toString() === serverMsg) {
-			/* start client */
+			//start client
 			console.log("server is ready..");
 			runClient();
 		} else {
