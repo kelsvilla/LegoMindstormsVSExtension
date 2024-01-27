0d8601775128126b099f97e0d867636fd8c18c26
Bryan Tang <bryanktang@gmail.com>
2024-01-27 17:25:52 -0600

    Add pyaudio and portaudio to dependecies folder; changed paths

--- voice-server-setup/initial_set_up_macos.sh
+++ voice-server-setup/initial_set_up_macos.sh
@@ -11,4 +11,4 @@ cd $ABSPATH
 python3 -m venv venv
 source venv/bin/activate
 which python
-LDFLAGS="-L/opt/homebrew/Cellar/portaudio/19.7.0/lib" CFLAGS="-I/opt/homebrew/Cellar/portaudio/19.7.0/include" pip install -r requirements.txt
+pip install -r requirements.txt
