d9ab373a5c049b25a04303263c9a0a87842d9c2a
K3V1N-G <123893629+K3V1N-G@users.noreply.github.com>
2024-02-04 14:20:13 -0600

    Added test cases, updated to use existing file for testing

--- src/test/suites/text/text.test.ts
+++ src/test/suites/text/text.test.ts
@@ -2,22 +2,12 @@ import * as assert from "assert";
 import * as vscode from "vscode";
 import { before, after } from "mocha";
 
-import { fetchLineNumber, getIndent } from "../../../commands/text";
+import * as command from "../../../commands/text";
 
-import * as fs from 'fs';
 import { join } from 'path';
 
-function syncWriteFile(filename: string, data: any) {
-  fs.writeFileSync(filename, data, {
-      flag: 'a+',
-  });
-}
-
 function initFile(): string {
-  const fileName = join(__dirname, "testing.py");
-  if (fs.existsSync(fileName)) {
-    fs.unlinkSync(fileName);
-  }
+  const fileName = join(__dirname, "../../../hubManager.js");
   return fileName;
 }
 
@@ -26,7 +16,7 @@ suite("Text Command Test Suite", () => {
 
   before(async () => {
     var fileName = initFile();
-    syncWriteFile(fileName, '');
+
     const document = await vscode.workspace.openTextDocument(fileName);
     editor = await vscode.window.showTextDocument(document);
   });
@@ -37,24 +27,43 @@ suite("Text Command Test Suite", () => {
 
   test("Get line number", () => {
       for(let i = 0; i <= 10; i++){
-        syncWriteFile("testing.py", '\n');
         editor.selection = new vscode.Selection(i, 0, i, 0);
-        let lineNumber = fetchLineNumber(vscode.window.activeTextEditor) - 1;
+        let lineNumber = command.fetchLineNumber(vscode.window.activeTextEditor) - 1;
         assert.equal(lineNumber, i);
       }
   });
+
   test("Get indent", () => {
-    for(let i = 1; i <= 10; i++){
-      fs.unlinkSync("testing.py");
-      for(let j = 0; j < i; j++){
-        syncWriteFile("testing.py", " ");
-      }
-      syncWriteFile("testing.py", "flavor");
-      editor.selection = new vscode.Selection(0, 1, 0, 1);
-      console.log(vscode.window.activeTextEditor?.document.getText());
-      let indentNum = getIndent();
-      console.log(indentNum);
-      assert.equal(i, indentNum);
-    }
+    editor.selection = new vscode.Selection(80, 0, 80, 0);
+    let indentNum = command.getIndent();
+    assert.equal(7, indentNum);
+  });
+
+  test("Get leading spaces", () => {
+    editor.selection = new vscode.Selection(80, 0, 80, 0);
+    assert.equal(28, command.fetchNumberOfLeadingSpaces(editor));
+  });
+
+  test("Get number of selected lines", () => {
+    editor.selection = new vscode.Selection(80, 0, 85, 0);
+    assert.equal(6, command.fetchNumberOfSelectedLines(editor));
+  });
+
+  test("Get line", () => {
+    editor.selection = new vscode.Selection(80, 0, 80, 0);
+    let currentLine = editor.document.lineAt(80);
+    assert.notStrictEqual(currentLine, command.fetchLine(editor));
+  });
+
+  test("Move Cursor to Beginning", () => {
+    editor.selection = new vscode.Selection(80, 0, 80, 0);
+    command.moveCursorBeginning();
+    assert.equal(0, editor.selection.active.line);
+  });
+
+  test("Move Cursor to End", () => {
+    editor.selection = new vscode.Selection(80, 0, 80, 0);
+    command.moveCursorEnd();
+    assert.equal(369, editor.selection.active.line);
   });
 });
