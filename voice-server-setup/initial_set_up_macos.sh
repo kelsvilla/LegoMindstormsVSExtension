9741b7f485de5e50e24ec180ab334fe7dc097a98
Bryan Tang <bryanktang@gmail.com>
2024-02-10 17:07:38 -0600

    add cflags to setup, add script to move lib files

--- voice-server-setup/initial_set_up_macos.sh
+++ voice-server-setup/initial_set_up_macos.sh
@@ -12,3 +12,11 @@ python3 -m venv venv
 source venv/bin/activate
 which python
 pip install -r requirements.txt
+
+cd $ROOTDIR/dependencies/portaudio
+./configure --disable-mac-universal && make
+cd $ROOTDIR/dependencies/portaudio/lib/.libs
+find . -maxdepth 1 -exec mv {} .. \;
+
+CFLAGS='-I${ROOTDIR}/dependencies/portaudio/include -L${ROOTDIR}/dependencies/portaudio/lib'
+pip install ${ROOTDIR}/dependencies/PyAudio-0.2.14
