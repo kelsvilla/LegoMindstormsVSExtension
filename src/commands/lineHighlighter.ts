691db1bf2b092772be6fa57d2df54729d6b3058e
Kelsee Villareal <118868414+kelsvilla@users.noreply.github.com>
2024-04-01 00:15:25 -0500

    Added line highlighter configurator

--- src/commands/lineHighlighter.ts
+++ src/commands/lineHighlighter.ts
@@ -37,10 +37,6 @@
 * "mind-reader.lineHighlighter.textDecoration"     : "none",
 * "mind-reader.lineHighlighter.textColor"          : "#FFFFFF",
 *
-* ! Restart VSCode for changes to take effect (if they didn't automatically)
-* ! Afterwards you can now edit using the settings window, or manually edit them
-* ! directly in settings.json by editing the values.
-*
 * TODO: FEATURE: Add ability for user to change options through a command pallette configurator
 * TODO: BUG: Adding the settings configurator made default settings break (if no values are found in settings.json)
 **/
@@ -53,6 +49,7 @@ import {
 	WorkspaceConfiguration,
 	Range,
 } from "vscode";
+import * as vscode from 'vscode';
 
 let highlightStyle: TextEditorDecorationType;
 
@@ -320,6 +317,95 @@ export function toggleLineHighlight() {
 	}
 }
 
+let currentPanel: vscode.WebviewPanel | undefined;
+
+export function changeHighlightColor(){
+	const columnToShowIn=vscode.window.activeTextEditor ? vscode.window.activeTextEditor.viewColumn : undefined;
+
+	if (!currentPanel){
+		currentPanel=vscode.window.createWebviewPanel(
+			'changeHighlightColor',
+			'Highlight Color Picker',
+			vscode.ViewColumn.One,
+			{
+				enableScripts: true,
+				retainContextWhenHidden: true,
+			}
+			);
+
+			const backgroundColor=vscode.workspace.getConfiguration('mind-reader.lineHighlighter').get<string>("backgroundColor");
+			const outlineColor=vscode.workspace.getConfiguration('mind-reader.lineHighlighter').get<string>("outlineColor");
+
+			currentPanel.webview.postMessage({backgroundColor, outlineColor});
+			currentPanel.webview.html=getWebviewContent();
+
+			currentPanel.onDidDispose(()=> {
+				currentPanel=undefined;
+			});	}
+	else{
+		currentPanel.reveal(columnToShowIn);
+	}
+
+	currentPanel.webview.onDidReceiveMessage(message => {
+		if (message.type === 'selectedColors'){
+			const bgColor=message.backgroundColor;
+			const olColor=message.outlineColor;
+
+			workspace.getConfiguration('mind-reader.lineHighlighter').update('backgroundColor', bgColor, true);
+			workspace.getConfiguration('mind-reader.lineHighlighter').update('outlineColor', olColor, true);
+		}
+	});
+}
+
+function getWebviewContent(){
+	return `<!DOCTYPE html>
+	<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>Hightlight Color Picker</title>
+	</head>
+	<body>
+		<h1>Choose colors for highlight tool:</h1>
+		<div>
+			<label for="body">Background Color</label>
+			<input type="color" id="body" name="body" />
+		</div>
+		<div>
+			<label for="outline">Outline Color</label>
+			<input type="color" id="outline" name="outline" />
+		</div>
+		<button onclick="sendSelectedColors()">Update Highlight Colors</button>
+		<script>
+		const vscode = acquireVsCodeApi();
+
+		window.addEventListener('message', event=> {
+			const colors=event.data;
+			const backgroundColor= colors.backgroundColor;
+			const outlineColor=colors.outlineColor;
+
+			document.getElementById('body').value=backgroundColor;
+			document.getElementById('outline').value=outlineColor;
+		});
+		</script>
+		<script>
+		function sendSelectedColors(){
+			const backgroundColor=document.getElementById('body').value;
+			const outlineColor=document.getElementById('outline').value;
+
+			const message = {
+				type: 'selectedColors',
+				backgroundColor: backgroundColor,
+				outlineColor: outlineColor
+			};
+
+			vscode.postMessage(message);
+		}
+		</script>
+	</body>
+	</html>`;
+}
+
 // Clean-up after ourselves
 export function highlightDeactivate() {
 	// when the plugin is terminated remove all highlighting
@@ -334,4 +420,8 @@ export const lineHighlightercommands: CommandEntry[] = [
 		name: "mind-reader.toggleLineHighlight",
 		callback: toggleLineHighlight,
 	},
+	{
+		name: "mind-reader.changeHighlightColor",
+		callback: ()=> changeHighlightColor(),
+	}
 ];
