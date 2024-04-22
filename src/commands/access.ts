3d03fb7c5937de0f3029c2b84f87779d52d6b4d9
Alex Tomjack <amt1309@gmail.com>
2024-04-22 01:39:01 -0500

    Began work on implementing previously unimplemented undo functions

--- src/commands/access.ts
+++ src/commands/access.ts
@@ -47,18 +47,40 @@ export const accessCommands: CommandEntry[] = [
   },
 ];
 
+let baseZoomLevel = vscode.workspace.getConfiguration("window").get<number>("zoomLevel") || 0;
+let fontZoomLevel = 0;
+let fontZoomBeforeReset = 0;
+
 function increaseFontScale(): void {
 	vscode.commands.executeCommand("editor.action.fontZoomIn");
+	fontZoomLevel += 1;
 	outputMessage("Font Scale Increased.");
 }
 
 function decreaseFontScale(): void {
 	vscode.commands.executeCommand("editor.action.fontZoomOut");
+	fontZoomLevel -= 1;
 	outputMessage("Font Scale Decreased.");
 }
 
 function resetFontScale(): void {
 	vscode.commands.executeCommand("editor.action.fontZoomReset");
+	fontZoomBeforeReset = fontZoomLevel;
+	fontZoomLevel = 0;
+	outputMessage("Font Scale Reset.");
+}
+
+async function undoResetFontScale(): Promise<void> {
+	if(fontZoomLevel > 0) {
+		for(let i = 0; i < fontZoomLevel; i++) {
+			vscode.commands.executeCommand("editor.action.fontZoomOut");
+		}
+	} else {
+		for(let i = 0; i < Math.abs(fontZoomLevel); i++) {
+			vscode.commands.executeCommand("editor.action.fontZoomIn");
+		}
+	}
+	fontZoomLevel = 0;
 	outputMessage("Font Scale Reset.");
 }
 
